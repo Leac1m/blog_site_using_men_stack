eb93f8585954e814a7701d30a19fbe806a533027
leac1m <obemichael10@gmail.com>
2024-11-13 09:59:15 +0100

    added WYSIWYG to post creation

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -1,6 +1,9 @@
 module.exports = (req, res) => {
-    if (req.session.userId)
-        return res.render('create')
+    if (req.session.userId) {
+        return res.render('create', {
+            createPost: true
+        })
+    }
 
     res.redirect('/auth/login')
 }
